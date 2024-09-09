e041f98d477044ef1f72a7c0a1f0ff72aa51eb79
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-09-08 21:30:57 -0700

    remove sudo zsh;  brew doesnt want elevated permissons on script

--- install/brewInstall.sh
+++ install/brewInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chmod a+x /$PWD/brewIn.sh
+chmod a+x /$PWD/brewInstall.sh
 chmod a+x /$PWD/brew_install.command
 sudo zsh  
 /$PWD/brew_install.command -a 2>&1 | tee -a /$PWD/logs/"brewInstall$(date +%F)@$(date +%H:%M:%S).txt"
